b179fc851b3a54d683a54b4439e374749b2b9879
Shaobin-Jiang <shaobin-jiang@outlook.com>
2025-04-29 23:12:11 +0800

    create qutebrowser config

---
+++ qutebrowser/.qutebrowser/config.py
@@ -0,0 +1,54 @@
+config.load_autoconfig(False)
+
+config.source("colorscheme.py")
+config.source("keymaps.py")
+
+c.content.pdfjs = True
+c.downloads.remove_finished = 3000
+
+# Otherwise qutebrowser cannot find kitty!!!
+# To check the PATH in qutebrowser, run `:"debug-pyeval os.environ['PATH']`
+import os
+old_path = os.environ["PATH"]
+os.environ["PATH"] = f"/opt/homebrew/bin:{old_path}"
+
+c.editor.command = [
+    "kitty",
+    "--single-instance",
+    "-d",
+    "~",
+    "-e",
+    "nvim",
+    "-f",
+    "{file}",
+    "-c",
+    "normal {line}G{column0}l",
+]
+
+c.fonts.default_size = "16pt"
+c.content.headers.accept_language = "zh-CN;q=0.9,en-US,en;q=0.8"
+c.content.headers.user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+
+c.hints.padding = {"bottom": 1, "left": 5, "right": 3, "top": 1}
+c.hints.chars = "asdfghjklzxcvbnm"
+c.hints.border = "none"
+
+c.scrolling.bar = "always"
+c.scrolling.smooth = True
+
+c.statusbar.padding = {"bottom": 2, "left": 0, "right": 5, "top": 2}
+c.statusbar.widgets = ["search_match", "text:|", "url", "text:|", "scroll"]
+
+c.tabs.padding = {"bottom": 5, "left": 10, "right": 10, "top": 5}
+
+c.url.default_page = "https://bing.com"
+c.url.searchengines = {"DEFAULT": "https://bing.com/search?q={}"}
+c.url.start_pages = "https://bing.com"
+
+aliases = {
+    "source": "config-source",
+}
+
+for alias in aliases:
+    c.aliases[alias] = aliases[alias]
+
---
+++ qutebrowser/.qutebrowser/keymaps.py
@@ -0,0 +1,70 @@
+unbind = [
+    "<Alt-1>",
+    "<Alt-2>",
+    "<Alt-3>",
+    "<Alt-4>",
+    "<Alt-5>",
+    "<Alt-6>",
+    "<Alt-7>",
+    "<Alt-8>",
+    "<Alt-9>",
+    "<Ctrl-s>",
+    "<Ctrl-w>",
+    "d",
+    "H",
+    "J",
+    "K",
+    "L",
+]
+
+keymap = {
+    "<Ctrl-1>": "tab-focus 1",
+    "<Ctrl-2>": "tab-focus 2",
+    "<Ctrl-3>": "tab-focus 3",
+    "<Ctrl-4>": "tab-focus 4",
+    "<Ctrl-5>": "tab-focus 5",
+    "<Ctrl-6>": "tab-focus 6",
+    "<Ctrl-7>": "tab-focus 7",
+    "<Ctrl-8>": "tab-focus 8",
+    "<Ctrl-9>": "tab-focus 9",
+    "<Ctrl-0>": "tab-focus -1",
+    "<Ctrl-h>": "tab-prev",
+    "<Ctrl-l>": "tab-next",
+    "<Ctrl-j>": "forward",
+    "<Ctrl-k>": "back",
+    "<F12>": "devtools",
+    "<Ctrl-c>": "stop",
+    "<Ctrl-f>": "hint all hover",
+    "<Ctrl-g>": "jseval -q document.activeElement.blur()",
+    "<Ctrl-Shift-g>": "click-element css body",
+    "<Ctrl-q>": "tab-close",
+    "<Ctrl-d>": "cmd-run-with-count 15 scroll down",
+    "<Ctrl-u>": "cmd-run-with-count 15 scroll up",
+    "<Ctrl-w>h": "devtools-focus",
+    "<Ctrl-w>j": "devtools-focus",
+    "<Ctrl-w>k": "devtools-focus",
+    "<Ctrl-w>l": "devtools-focus",
+}
+
+# A joke really. Just using keymaps to quickly jump to new pages. Since it is
+# still way quicker than qutebrowser's quickmark, why not name it really quick
+# mark?
+# Rules here: the lhs provided here is actually the suffix. Prefixing it with
+# <Space> should open the link in a new tab whilst prefixing it with a comma
+# should open the link in the current window.
+really_quick_marks = {
+    "gh": "https://github.com",
+    "zh": "https://zhihu.com",
+    "bl": "https://shaobin-jiang.github.io/blog",
+    "ds": "https://chat.deepseek.com",
+}
+
+for unbind_lhs in unbind:
+    config.unbind(unbind_lhs)
+
+for lhs, rhs in keymap.items():
+    config.bind(lhs, rhs)
+
+for suffix, url in really_quick_marks.items():
+    config.bind(f"<Space>{suffix}", f"open -t {url}")
+    config.bind(f",{suffix}", f"open {url}")
