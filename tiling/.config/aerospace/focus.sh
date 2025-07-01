6d86aae1e5a11bcd6649b5cffbdc7addec8217c7
Shaobin-Jiang <shaobin-jiang@outlook.com>
2025-07-01 13:54:12 +0800

    aerospace: focus running app instead of starting a new instance

---
+++ tiling/.config/aerospace/focus.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PATTERN="$1"
+TARGET="$2"
+
+# 获取匹配的窗口ID
+window_id=$(aerospace list-windows --all | awk -v name="$PATTERN" '/^[0-9]+[ ]+\|[ ]+name/ {print $1}')
+
+if [ -n "$window_id" ]; then
+    aerospace focus --window-id "$window_id"
+else
+    open "$TARGET"
+fi
