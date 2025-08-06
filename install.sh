3283bdea40e039521d0e5cec2291558a3641b7c8
Shaobin-Jiang <shaobin-jiang@outlook.com>
2025-08-06 14:52:34 +0800

    add tmux

--- install.sh
+++ install.sh
@@ -7,11 +7,12 @@ install() {
     stow -t "$target_path" "$package_name"
 }
 
+install $HOME/.config/aerospace aerospace
 install $HOME/.config/fastfetch fastfetch
 install $HOME/.config/kitty kitty
 install $HOME/.qutebrowser qutebrowser
 install $HOME/.config starship
-install $HOME/.config/aerospace aerospace
 install $HOME/.config/sketchybar sketchybar
+install $HOME tmux
 install $HOME/.config/yazi yazi
 install $HOME zsh
