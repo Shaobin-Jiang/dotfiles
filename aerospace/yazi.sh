9b77e320b8a743a8567afe1b48b6e55f7f667522
Shaobin-Jiang <shaobin-jiang@outlook.com>
2025-08-15 13:50:42 +0800

    aerospace: quit kitty also when done with yazi

--- aerospace/yazi.sh
+++ aerospace/yazi.sh
@@ -8,5 +8,8 @@ window_id=$(aerospace list-windows --all | awk '/Yazi:/ {print $1}')
 if [ -n "$window_id" ]; then
     aerospace focus --window-id "$window_id"
 else
-    kitty --single-instance -d ~ -e yazi
+    # Use applescript to also quit kitty completely when exiting yazi
+    # Note that using a simple 'quit app "kitty"' would throw a 128 error
+    # Which is why we are using a complicated check syntax here
+    kitty -e bash -c $'yazi ~; osascript -e \'try\' -e \'quit app "kitty"\' -e \'on error errMsg number errNum\' -e \'if errNum ≠ -128 then\' -e \'display dialog "Error: " & errMsg\' -e \'end if\' -e \'end try\''
 fi
