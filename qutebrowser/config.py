8aa4fb8f01323275be046b2fcb9af98bcf2a78d5
Shaobin-Jiang <shaobin-jiang@outlook.com>
2025-08-21 15:25:32 +0800

    qutebrowser: fix user agent

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -29,7 +29,7 @@ c.editor.command = [
 c.fonts.default_size = "16pt"
 
 c.content.headers.accept_language = "zh-CN;q=0.9,en-US,en;q=0.8"
-c.content.headers.user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+c.content.headers.user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:141.0) Gecko/20100101 Firefox/141.0"
 c.content.javascript.clipboard = "access-paste"
 
 c.hints.padding = {"bottom": 1, "left": 5, "right": 3, "top": 1}
