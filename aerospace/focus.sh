461dd92310f58fa16e10412603fe992c2d5fdff0
Shaobin-Jiang <shaobin-jiang@outlook.com>
2025-08-29 19:23:41 +0800

    aerospace: focus.sh not working correctly
    It was never right to use `awk -v` that way. Stupid AI.

--- aerospace/focus.sh
+++ aerospace/focus.sh
@@ -3,7 +3,7 @@
 PATTERN="$1"
 TARGET="$2"
 
-window_id=$(aerospace list-windows --all | awk -v name="$PATTERN" '/^[0-9]+[ ]+\|[ ]+name/ {print $1}')
+window_id=$(aerospace list-windows --all | awk "/^[0-9]+[ ]+\|[ ]+$PATTERN/ {print \$1}")
 
 if [ -n "$window_id" ]; then
     aerospace focus --window-id "$window_id"
